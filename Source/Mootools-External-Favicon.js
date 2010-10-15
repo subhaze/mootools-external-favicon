a4403f61f4dacac6ab3539c74b05db4f01a93ef5
subhaze <jmrussell22@gmail.com>
2010-10-15 12:07:03 -0500

    - code clean up

--- Source/Mootools-External-Favicon.js
+++ Source/Mootools-External-Favicon.js
@@ -20,55 +20,54 @@ authors: [Michael Russell]
 */
 
 Element.implement({
+    getFavicons: function( imgLayout, className, imgExtensions ) {
+        var baseHost = URI.base.get('host'),
+            externalLinks = this.getElements( 'a[href^="http://"], a[href^="https://"]' ),
+            imgTypes = imgExtensions || ['ico', 'png', 'gif', 'bmp'],
+            imgSize = imgLayout || {'width':16,'height':16,'top':'4px','margin-right':'4px'};
+        externalLinks.each( function( a ) {
+            var alternateLink = $(a).get('class').match(/favicon\[(.+)\]/),
+                uri = new URI( a ),
+                domain, wrapper;
+            if( baseHost != uri.get( 'host' ) ) domain = uri.get('scheme')+'://' + uri.get('host');
+            if( className ) a.addClass( className );
 
-  getFavicons: function( imgLayout, className, imgExtensions ) {
-    var baseHost = URI.base.get('host'),
-        externalLinks = this.getElements( 'a[href^="http://"], a[href^="https://"]' ),
-        imgTypes = imgExtensions || ['ico', 'png', 'gif', 'bmp'],
-        imgSize = imgLayout || {'width':16,'height':16,'top':'4px','margin-right':'4px'};
-    externalLinks.each( function( a ) {
-      var alternateLink = $(a).get('class').match(/favicon\[(.+)\]/),
-          uri = new URI( a ),
-          domain, wrapper;
-
-      if( baseHost != uri.get( 'host' ) ) {
-        domain = uri.get('scheme')+'://' + uri.get('host');
-        if( className ) a.addClass( className );
-      }
-      wrapper = (a.style.display == 'block') ? new Element('div') : new Element('span');
-      wrapper.wraps( a );
-      if( alternateLink ){
-        new Element( 'img', {
-            'width': imgSize.width,
-            'height': imgSize.height,
-            'src': alternateLink[1],
-            'styles':{'margin-right':'4px', position:'relative', bottom:'-2px'}
-        }).inject( wrapper, 'top' );
-        return;
-      }
-
-      (function( i ){
-          var args = arguments,
-              favLink;
-          if( i >= imgTypes.length ) return;
-          favLink = domain +'/favicon.'+ imgTypes[i];
-          Asset.image( favLink, {
-            onload:   function() {
-                new Element('img', {
-                    'width':imgSize.width,
-                    'height':imgSize.height,
-                    'src':favLink,
+            wrapper = (a.style.display == 'block') ? new Element('div') : new Element('span');
+            wrapper.wraps( a );
+            if( alternateLink ){
+                new Element( 'img', {
+                    'width':    imgSize.width,
+                    'height':   imgSize.height,
+                    'src':      alternateLink[1],
                     'styles':{
-                        'margin-right':'4px',
-                        'position':'relative',
-                        'top': '3px'
+                        'margin-right': '4px',
+                        'position':     'relative',
+                        'bottom':       '-2px'
                     }
-                }).inject(wrapper,'top');
-            },
-            onerror:  function() { args.callee( ++i ) }
-          });
-        }
-      )(0)
-    });
-  }
+                }).inject( wrapper, 'top' );
+                return;
+            }
+
+            (function( i ){
+                var args = arguments,
+                    favLink = domain +'/favicon.'+ imgTypes[i];
+                if( i >= imgTypes.length ) return;
+                Asset.image( favLink, {
+                    onload:   function() {
+                        new Element('img', {
+                            'width':    imgSize.width,
+                            'height':   imgSize.height,
+                            'src':      favLink,
+                            'styles':{
+                                'margin-right': '4px',
+                                'position':     'relative',
+                                'top':          '3px'
+                            }
+                        }).inject(wrapper,'top');
+                    },
+                    onerror:  function() { args.callee( ++i ) }
+                });
+            })(0)
+        });
+    }
 });
