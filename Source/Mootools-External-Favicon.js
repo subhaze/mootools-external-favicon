230fb3f2560dbfb9f61ad536d889aac3187e931a
subhaze <jmrussell22@gmail.com>
2011-04-14 20:31:43 -0500

    white space clean up, removed usage of arguments.calle, now using data- attribute instead of abusing class attribute

--- Source/Mootools-External-Favicon.js
+++ Source/Mootools-External-Favicon.js
@@ -20,40 +20,40 @@ authors: [Michael Russell]
 */
 
 Element.implement({
-    getFavicons: function( classNamePrefix, imgExtensions ) {
+    getFavicons: function(classNamePrefix, imgExtensions){
         var baseHost = URI.base.get('host'),
-            externalLinks = this.getElements( 'a[href^="http://"], a[href^="https://"]' ),
+            externalLinks = this.getElements('a[href^="http://"], a[href^="https://"]'),
             imgTypes = imgExtensions || ['ico', 'png', 'gif', 'bmp'],
             classPrefix = classNamePrefix || '';
 
-        externalLinks.each( function( a ) {
-            var alternateLink = $(a).get('class').match(/favicon\[(.+)\]/),
-                uri = new URI( a ),
+        externalLinks.each(function(a){
+            var alternateLink = $(a).get('data-linkUrl'),
+                uri = new URI(a),
                 domain, wrapper;
 
-            if( baseHost != uri.get( 'host' ) ) domain = uri.get('scheme')+'://' + uri.get('host');
+            if(baseHost != uri.get('host')) domain = uri.get('scheme')+'://' + uri.get('host');
 
-            wrapper = new Element('span', {'class':classPrefix + 'favicon-wrapper'} ).wraps( a );
-            if( alternateLink ){
-                new Element( 'img', {
+            wrapper = new Element('span', {'class':classPrefix + 'favicon-wrapper'}).wraps(a);
+            if(alternateLink){
+                new Element('img',{
                     'class':    classPrefix + 'favicon-img',
-                    'src':      alternateLink[1]
-                }).inject( wrapper, 'top' );
+                    'src':      alternateLink
+                }).inject(wrapper, 'top');
                 return;
             }
 
-            (function( i ){
+            (function recurse(i){
                 var args = arguments,
                     favLink = domain +'/favicon.'+ imgTypes[i];
-                if( i >= imgTypes.length ) return;
-                Asset.image( favLink, {
-                    onload:   function() {
+                if(i >= imgTypes.length) return;
+                Asset.image(favLink, {
+                    onload:   function(){
                         new Element('img', {
                             'class':    classPrefix + 'favicon-img',
                             'src':      favLink
                         }).inject(wrapper,'top');
                     },
-                    onerror:  function() { args.callee( ++i ) }
+                    onerror:  function(){ recurse( ++i ) }
                 });
             })(0)
         });
